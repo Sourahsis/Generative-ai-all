ec1ce2030228e38b327a12e6ec59e5ae2ef6d9ee
Sourashis Sarkar <116754962+Sourahsis@users.noreply.github.com>
2024-03-05 11:00:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -95,9 +95,8 @@ def chat_with_pdf():
     if(user_question):
         user_question = str(user_question) 
         language=detect_language_code(user_question)
-        user_question=TextBlob(language)
         if (blob.detect_language() != 'en'):
-          user_question=user_question.translate(to='en')
+          user_question=translator.translate(user_question, dest='en').text
     if user_question:
         user_input(user_question,language)
     with st.sidebar:
